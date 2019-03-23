564805c3d1d9e405e945e092a33188a767174c67
Onterio Wright <onteriowright@yahoo.com>
2019-03-23 14:34:54 -0500

    made changes

--- members.js
+++ members.js
@@ -1,14 +1,9 @@
-var membersSlideIndex = 0;
-bandSlide();
-
-function bandSlide() {
-  var i;
-  var slide = document.getElementsByClassName("bandSlide");
-  for (i = 0; i < slide.length; i++) {
-    slide[i].style.display = "none";  
-  }
-  membersSlideIndex++;
-  if (membersSlideIndex > slide.length) {membersSlideIndex = 1}    
-  slide[membersSlideIndex-1].style.display = "block";  
-  setTimeout(bandSlide, 3000); // Change image every 3 seconds
+function bandSlide(change) {
+	var slide = document.getElementsByClassName("bandSlide");
+	slide.forEach(s => (s.style.display = "none"));
+	slide[change].style.display = "block";
+	change = change === slide.length - 1 ? 0 : change + 1;
+	setTimeout(() => bandSlide(change), 3000);
 }
+
+bandSlide(0);
